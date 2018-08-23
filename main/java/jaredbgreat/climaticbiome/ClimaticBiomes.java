aa93eb412fe9dab065352864a9daa40659740b74
Jared Blackburn (JaredBGreat) <BlackJar72@yahoo.com>
2018-08-23 00:07:36 -0400

    Minor corrections

--- main/java/jaredbgreat/climaticbiome/ClimaticBiomes.java
+++ main/java/jaredbgreat/climaticbiome/ClimaticBiomes.java
@@ -12,8 +12,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid=Info.ID, name=Info.NAME, version=Info.VERSION, 
-		acceptableRemoteVersions=Info.VERSION, dependencies="after:biomesoplenty") 
+@Mod(modid=Info.ID, name=Info.NAME, version=Info.VERSION,
+		acceptableRemoteVersions=Info.VERSION, dependencies="after:biomesoplenty")
 public class ClimaticBiomes {
 	public static ClimaticBiomes instance;
 	public static BetterWorldType worldType;
@@ -22,33 +22,33 @@ public class ClimaticBiomes {
 	@SidedProxy(clientSide = "jaredbgreat.climaticbiome.proxy.ClientProxy",
 			    serverSide = "jaredbgreat.climaticbiome.proxy.ServerProxy")
 	public static IProxy proxy;
-	
-    
+
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
     	instance = this;
-    	//configHandler = new ConfigHandler(event.getModConfigurationDirectory().toPath() 
+    	//configHandler = new ConfigHandler(event.getModConfigurationDirectory().toPath()
     	//		+ File.separator + Info.DIR);
     	BlockRegistrar.initBlocks();
     	ItemRegistrar.initItems();
     	worldType = new BetterWorldType();
     	ModBiomes.createBiomes();
     }
-    
-    
-    @EventHandler 
-    public void init(FMLInitializationEvent event) {}
-    
-    
+
+
     @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {    	
+    public void init(FMLInitializationEvent event) {}
+
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
     	//configHandler.findCustomBiomes();
     }
-    
-    
+
+
     private static boolean bopLoaded() {
-		return net.minecraftforge.fml.common.Loader.isModLoaded("biomesoplenty");	
+		return net.minecraftforge.fml.common.Loader.isModLoaded("biomesoplenty");
 	}
-	
+
 
 }
--- main/java/jaredbgreat/climaticbiome/generation/cache/Cache.java
+++ main/java/jaredbgreat/climaticbiome/generation/cache/Cache.java
@@ -1,13 +1,8 @@
 package jaredbgreat.climaticbiome.generation.cache;
 
-// Something about this clearly isn't working as expected; adding 
-// extra data seems to prevent somethings from being retrieved 
-// and/or added -- which makes no sense, so I have no idea what 
-// I've done wrong.
-
 /**
- * A cache system using a skiplist based hash map.
- * 
+ * A cache system using a based hash map.
+ *
  * @author Jared Blackburn
  * @param <T>
  */
@@ -17,8 +12,8 @@ public class Cache <T extends ICachable> {
     private int capacity;
     private int lowLimit;
     private int length;
-    
-    
+
+
     /**
      * Creates a cache with a default starting size elements.
      */
@@ -29,8 +24,8 @@ public class Cache <T extends ICachable> {
         lowLimit = ((size - minSize) * 3) / 16;
         length = 0;
     }
-    
-    
+
+
     /**
      * Creates a cache with a default starting size of 16 elements.
      */
@@ -41,11 +36,11 @@ public class Cache <T extends ICachable> {
         lowLimit = 0;
         length = 0;
     }
-    
-    
+
+
     /**
      * At a new element to the cache.
-     * 
+     *
      * @param item the object to be added.
      */
     public void add(T item) {
@@ -69,8 +64,8 @@ public class Cache <T extends ICachable> {
             }
         }
     }
-    
-    
+
+
     /**
      * Return the element at the given Coords.
      * @param coords
@@ -89,14 +84,14 @@ public class Cache <T extends ICachable> {
             } else {
                 offset++;
             }
-        }        
+        }
         return null;
     }
-    
-    
+
+
     /**
      * Return the element at the given coordinate values x and z.
-     * 
+     *
      * @param x
      * @param z
      * @return the object stored for those coordinates, or null.
@@ -114,16 +109,16 @@ public class Cache <T extends ICachable> {
             } else {
                 offset++;
             }
-        }        
+        }
         return null;
     }
-    
-    
+
+
     /**
      * Will tell if an item for the given coordinates is in the cache.
-     * 
+     *
      * @param coords
-     * @return 
+     * @return
      */
     public boolean contains(Coords coords) {
         int bucket = (coords.hashCode() & 0x7fffffff) % data.length;
@@ -137,17 +132,17 @@ public class Cache <T extends ICachable> {
             } else {
                 offset++;
             }
-        }        
+        }
         return false;
     }
-    
-    
+
+
     /**
      * Will tell if an item for the given coordinates is in the cache.
-     * 
+     *
      * @param x
      * @param z
-     * @return 
+     * @return
      */
     public boolean contains(int x, int z) {
         int bucket = (Coords.hashCoords(x, z) & 0x7fffffff) % data.length;
@@ -161,16 +156,16 @@ public class Cache <T extends ICachable> {
             } else {
                 offset++;
             }
-        }        
+        }
         return false;
     }
-    
-    
+
+
     /**
      * Will tell if an item the same coords is in the cache.
-     * 
+     *
      * @param in
-     * @return 
+     * @return
      */
     public boolean contains(T in) {
         Coords coords = in.getCoords();
@@ -185,11 +180,11 @@ public class Cache <T extends ICachable> {
             } else {
                 offset++;
             }
-        }        
+        }
         return false;
     }
-    
-    
+
+
     /**
      * This will grow the data size when needed.
      */
@@ -204,8 +199,8 @@ public class Cache <T extends ICachable> {
         capacity = (data.length * 3) / 4;
         lowLimit = ((data.length - minSize) * 3) / 16;
     }
-    
-    
+
+
     /**
      * This will shrink the data size when needed.
      */
@@ -220,8 +215,8 @@ public class Cache <T extends ICachable> {
         capacity = (data.length * 3) / 4;
         lowLimit = ((data.length - minSize) * 3) / 16;
     }
-    
-    
+
+
     private void rebucket(ICachable item) {
         int bucket = (item.getCoords().hashCode() & 0x7fffffff) % data.length;
         int offset = 0;
@@ -235,12 +230,12 @@ public class Cache <T extends ICachable> {
             }
         }
     }
-    
-    
+
+
     /**
-     * This will iterate the cache items and remove any that identify 
- themselves as isOldData().  Usually this should mean removing items from 
- the cache that haven't been used in a set amount of time (most often 
+     * This will iterate the cache items and remove any that identify
+ themselves as isOldData().  Usually this should mean removing items from
+ the cache that haven't been used in a set amount of time (most often
  30 seconds), though other criteria for isOldData() could be created.
      */
     public void cleanup() {
@@ -263,5 +258,5 @@ public class Cache <T extends ICachable> {
         		}
         	}
         }
-    } 
+    }
 }
